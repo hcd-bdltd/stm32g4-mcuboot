53c4ce5830ef9d032e3a079b5abc164fbf7d8c3f
Hudson C. Dalpra <hudson@bduncanltd.com>
2024-03-14 10:32:41 +1300

    fixed: check script args

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-set -eux -o pipefail
+set -eu -o pipefail
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <project_name>"
+    exit 1
+fi
+
+test -f "$1/Makefile" || {
+    echo "$1/Makefile does not exist"
+    exit 1
+}
 
 cd $1
 make clean all
--- flash.sh
+++ flash.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -eux -o pipefail
+set -eu -o pipefail
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <project_name>"
+    exit 1
+fi
+
+test -f "$1/out/$1.bin" || {
+    echo "$1/out/$1.bin does not exist"
+    exit 1
+}
 
 st-flash write $1/out/$1.bin 0x08000000
 st-flash reset
